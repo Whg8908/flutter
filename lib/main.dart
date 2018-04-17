aa09b17a4e8e5cb9b95e314995a205da3a7dc42d
whg <ghw8908@163.com>
2018-04-17 21:49:29 +0800

    03 创建一个无限滚动的ListView

--- lib/main.dart
+++ lib/main.dart
@@ -9,17 +9,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 //    final wordPair = new WordPair.random();
     return new MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Welcome to Flutter'),
-        ),
-        body: new Center(
-//          child: new Text('Hello World'),
-//          child: new Text(wordPair.asPascalCase),
-        child: new RandomWords(),
-        ),
-      ),
+      title: 'Startup Name Generator',
+//      home: new Scaffold(
+////        appBar: new AppBar(
+////          title: new Text('Welcome to Flutter'),
+////        ),
+////        body: new Center(
+//////          child: new Text('Hello World'),
+//////          child: new Text(wordPair.asPascalCase),
+////          child: new RandomWords(),
+////        ),
+//      ),
+      home: new RandomWords(),
       debugShowCheckedModeBanner: false, //去掉slow mode
     );
   }
@@ -31,9 +32,52 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  //以_命名就是表示私有变量
+  final _suggestions = <WordPair>[];
+
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
+//    final wordPair = new WordPair.random();
+//    return new Text(wordPair.asPascalCase);
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('StartUp Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        // 对于每个建议的单词对都会调用一次itemBuilder，然后将单词对添加到ListTile行中
+        // 在偶数行，该函数会为单词对添加一个ListTile row.
+        // 在奇数行，该行书湖添加一个分割线widget，来分隔相邻的词对。
+        // 注意，在小屏幕上，分割线看起来可能比较吃力。
+        itemBuilder: (context, i) {
+          // 在每一列之前，添加一个1像素高的分隔线widget
+          if (i.isOdd) return new Divider();
+
+          // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
+          // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
+          final index = i ~/ 1;
+
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
